36d30b4218e54c39cff3fa39da79cfd5483a9952
Phil <natsudragoneel282003@gmail.com>
2026-07-20 18:32:25 -0700

    Imported BrowserRouter to provide routing navigation for the user

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
+
 import App from './App.jsx'
+import Navbar from './pages/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path ="/" element={<Navbar/>} /> 
+        <Route index={true} element={<App/>} />
+      <Route />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 )
