1ffecefc4426d19293c91a9b7951f7bf58241614
Phil <natsudragoneel282003@gmail.com>
2026-07-21 15:14:32 -0700

    Added View Your Crewmates section on the navigation bar to view total crewmates made

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Navlink, NavLink } from 'react-router'
+import { Outlet, NavLink } from 'react-router'
 
 function Navbar() {
     return(
@@ -6,6 +6,7 @@ function Navbar() {
             <nav className="sidebar">
                 <NavLink to ="/" end className="sidebar-link"> 🛸 Home Base </NavLink>
                 <NavLink to ="create" className="sidebar-link"> 👽 Create a Crewmate! </NavLink>
+                <NavLink to ="gallery" className="sidebar-link"> 🖼️ View Your Crewmates!</NavLink>
             </nav>
             <main className="page-content">
                 <div className="page-inner">
