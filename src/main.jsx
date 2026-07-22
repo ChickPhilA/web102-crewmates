f770b64262cc755169ecf8d8eb4394df83fc3668
Phil <natsudragoneel282003@gmail.com>
2026-07-21 21:04:04 -0700

    Added a Route for a crewmate's detals

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Navbar from './pages/Navbar.jsx'
 import CreateCrewmate from './pages/createCrewmate.jsx'
 import ReadCrewmates from './pages/ReadCrewmates.jsx'
 import EditCrewmate from './pages/EditCrewmate.jsx'
+import DetailCrewmate from './pages/DetailCrewmate.jsx'
 import NotFound from './pages/NotFound.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')).render(
         <Route index={true} element={<App/>} />
         <Route path="create" element={<CreateCrewmate/>} />
         <Route path="gallery" element={<ReadCrewmates/>} />
+        <Route path="crewmate/:id" element={<DetailCrewmate/>} />
         <Route path="crewmate/:id/edit" element={<EditCrewmate/>} />
       </Route>
       <Route path="*" element={<NotFound />} />
